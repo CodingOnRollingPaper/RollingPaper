8bef7ace0ec16953859046c3298188f29f22a2b8
오성인 <dhtjddls12@naver.com>
2022-12-01 17:45:26 +0900

    기본 페이지 만드는 중~

--- Controller/Cmain.js
+++ Controller/Cmain.js
@@ -1,8 +1,21 @@
 const models = require('../models');
-//  login
+
+// 메인 페이지 GET
 exports.getMain = (req, res) => {
     res.render('index');
 }
 
+// 로그인 페이지 GET
+exports.getLogin = (req, res) => {
+    res.render('login');
+}
 
+// 로그인 페이지 로그인 POST 
+exports.postLogin = (req, res) => {
+    
+}
 
+// 회원가입 페이지 렌더링 GET
+exports.getSignup = (req, res) => {
+    res.render('signup');
+}
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,22 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controller/Cmain');
 
+
+// 메인 페이지 GET
 router.get('/', controller.getMain);
+
+// 로그인 페이지 렌더링 GET
+router.get('/login', controller.getLogin);
+// 로그인 페이지 로그인 POST
+router.post('/login', controller.postLogin);
+
+// 회원가입 페이지 렌더링 GET
+router.get('/signup', controller.getSignup);
+
+
+
+
+
 
 
 module.exports = router;
